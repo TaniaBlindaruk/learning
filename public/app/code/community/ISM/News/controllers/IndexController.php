12e5d35021caaf8725b7a686872bd720e3602064
Tania Blindaruk <t.blindaruk@gmail.com>
2015-04-24 15:27:27 +0300

    habr 6

--- public/app/code/community/ISM/News/controllers/IndexController.php
+++ public/app/code/community/ISM/News/controllers/IndexController.php
@@ -5,6 +5,16 @@ class ISM_News_IndexController extends Mage_Core_Controller_Front_Action
 
     public function indexAction()
     {
+        $resource = Mage::getSingleton('core/resource');
+        $read = $resource->getConnection('core_read');
+        $table = $resource->getTableName('news/entity');
+
+        $select = $read->select()
+            ->from($table, array('news_id', 'title', 'content'));
+
+        $news = $read->fetchAll($select);
+        Mage::register('news', $news);
+
         $this->loadLayout();
         $this->renderLayout();
     }
