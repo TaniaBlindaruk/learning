97febb7d2722546ffedf55f6af0eac9bddc3d700
Tania Blindaruk <t.blindaruk@gmail.com>
2015-04-24 18:30:05 +0300

    Some Change

--- public/app/code/community/ISM/News/Block/News.php
+++ public/app/code/community/ISM/News/Block/News.php
@@ -5,7 +5,9 @@ class ISM_News_Block_News extends Mage_Core_Block_Template
 
     public function getNewsCollection()
     {
+        /** @var ISM_News_Model_Resource_News_Collection $newsCollection */
         $newsCollection = Mage::getModel('news/news')->getCollection();
+//        $newsCollection->addFieldToFilter();
         return $newsCollection;
     }
 
