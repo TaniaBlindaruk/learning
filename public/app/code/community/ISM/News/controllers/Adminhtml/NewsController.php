23987f4f234cd9d3a746ddbc7d4b5c673570853a
Tania Blindaruk <t.blindaruk@gmail.com>
2015-04-24 13:13:18 +0300

    ADaminhtml add block

--- public/app/code/community/ISM/News/controllers/Adminhtml/NewsController.php
+++ public/app/code/community/ISM/News/controllers/Adminhtml/NewsController.php
@@ -5,7 +5,11 @@ class ISM_News_Adminhtml_NewsController extends Mage_Adminhtml_Controller_Action
 
     public function indexAction()
     {
-        echo '<h1>News Module: Admin section</h1>';
+        $this->loadLayout();
+        $this->_setActiveMenu('news');
+        $contentBlock = $this->getLayout()->createBlock('news/adminhtml_news');
+        $this->_addContent($contentBlock);
+        $this->renderLayout();
     }
 
 }
