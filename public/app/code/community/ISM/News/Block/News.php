ef95e3c3e8aa35313927bc1ea55777f5c1245f25
Tania Blindaruk <t.blindaruk@gmail.com>
2015-04-26 16:10:37 +0300

    .add file

--- public/app/code/community/ISM/News/Block/News.php
+++ public/app/code/community/ISM/News/Block/News.php
@@ -2,7 +2,6 @@
 
 class ISM_News_Block_News extends Mage_Core_Block_Template
 {
-
     public function getNewsCollection()
     {
         /** @var ISM_News_Model_Resource_News_Collection $newsCollection */
