9e5cad080819f14ec753b0b5475666ab5eb5ac9d
Tania Blindaruk <t.blindaruk@gmail.com>
2015-04-27 13:45:18 +0300

    Create a frontend controller to allow visitors review news announces and news detail page. /+

--- public/app/code/community/ISM/News/Block/News.php
+++ public/app/code/community/ISM/News/Block/News.php
@@ -5,6 +5,7 @@ class ISM_News_Block_News extends Mage_Core_Block_Template
     public function getNewsCollection()
     {
         /** @var ISM_News_Model_Resource_News_Collection $newsCollection */
+//        AND publish_date >= "' . date("Y/m/d") . '"'
         $newsCollection = Mage::getModel('news/news')->getCollection();
         $newsCollection->myFilter();
         return $newsCollection;
