73501a5b5cd996759b98007f60f6fc26d29f7a51
Tania Blindaruk <t.blindaruk@gmail.com>
2015-04-29 00:50:41 +0300

    The “Deliver at” value should be stored in the quote and order tables, use installers to update database scheme.

---
+++ public/app/code/community/ISM/DeliveryAt/Helper/Data.php
@@ -0,0 +1,6 @@
+<?php
+
+class ISM_DeliverAt_Helper_Data extends Mage_Core_Helper_Abstract
+{
+
+}
---
+++ public/app/code/community/ISM/DeliveryAt/sql/deliveryat_setup/install-0.1.0.php
@@ -0,0 +1,22 @@
+<?php
+
+$installer = $this;
+$installer->startSetup();
+$installer->getConnection()
+        ->addColumn(
+                "sales_flat_order", "deliverydate", array(
+            'nullable' => true,
+            'type' => Varien_Db_Ddl_Table::TYPE_DATE,
+            'comment' => 'Delivery At'
+                )
+
+);
+$installer->getConnection()
+        ->addColumn(
+                "sales_flat_quote", "deliverydate", array(
+            'nullable' => true,
+            'type' => Varien_Db_Ddl_Table::TYPE_DATE,
+            'comment' => 'Delivery At'
+                )
+);
+$installer->endSetup();
