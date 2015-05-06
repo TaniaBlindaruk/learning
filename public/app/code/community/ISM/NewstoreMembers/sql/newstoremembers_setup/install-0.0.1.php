b86b42f6fbc3c85da202107213d5b4b23742af3b
t.blindaruk <t.blindaruk@gmail.com>
2015-05-06 18:20:28 +0300

    Deleted 3 sql

--- public/app/code/community/ISM/NewstoreMembers/sql/newstoremembers_setup/install-0.0.1.php
+++ public/app/code/community/ISM/NewstoreMembers/sql/newstoremembers_setup/install-0.0.1.php
@@ -1,5 +1,5 @@
 <?php
-
+/** @var Mage_Core_Model_Resource_Setup $installer */
 $installer = $this;
 $tableNews = $installer->getTable('newstoremembers/table_newstoremembers');
 $installer->startSetup();
@@ -23,12 +23,13 @@ $table = $installer->getConnection()
         'nullable'  => true,
     ))
     ->addForeignKey(
-        $installer->getFkName('newstoremembers/table_newstoremembers', 'id', 'customer/entity', 'entity_id'),
-        'id',
+        $installer->getFkName('newstoremembers/table_newstoremembers', 'customer_id', 'customer/entity', 'entity_id'),
+        'customer_id',
         $installer->getTable('customer/entity'),
         'entity_id',
-        Varien_Db_Ddl_Table::ACTION_CASCADE,
-        Varien_Db_Ddl_Table::ACTION_CASCADE);
+        Varien_Db_Ddl_Table::ACTION_SET_NULL,
+        Varien_Db_Ddl_Table::ACTION_SET_NULL);
+
 $installer->getConnection()->createTable($table);
 
 $installer->endSetup();
