09ad8e665eea74a9b237054a5bead81f8453d9d3
t.blindaruk <t.blindaruk@gmail.com>
2015-05-06 18:49:26 +0300

    Nuwstore members:Us to OptionArray

--- public/app/code/community/ISM/NewstoreMembers/Helper/Data.php
+++ public/app/code/community/ISM/NewstoreMembers/Helper/Data.php
@@ -6,19 +6,11 @@ class ISM_NewstoreMembers_Helper_Data extends Mage_Core_Helper_Abstract
         /** @var Mage_Customer_Model_Resource_Customer_Collection $customers */
         $customers = Mage::getModel('customer/customer')->getCollection()->addNameToSelect();
 
-        $customers->toOptionArray();
-
         $result[]=array(
             'value'=>null,
             'label'=>'-------------------'
         );
-        foreach($customers as $customer){
-            $push = array(
-                'value'=>$customer->getId(),
-                'label'=>$customer->getName()
-            );
-            $result[]=$push;
-        }
+        $result  = array_merge($result,$customers->toOptionArray());
         return $result;
     }
 }
