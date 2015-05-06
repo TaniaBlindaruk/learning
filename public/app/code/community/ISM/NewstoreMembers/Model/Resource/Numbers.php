650f4998aa4fabdd155000fb4f32e0f68be71562
Tania Blindaruk <t.blindaruk@gmail.com>
2015-05-07 00:53:58 +0300

    NewstoreMembers: Видаленно mysql4

--- public/app/code/community/ISM/NewstoreMembers/Model/Resource/Numbers.php
+++ public/app/code/community/ISM/NewstoreMembers/Model/Resource/Numbers.php
@@ -1,6 +1,6 @@
 <?php
 
-class ISM_NewstoreMembers_Model_Resource_Numbers extends Mage_Core_Model_Mysql4_Abstract
+class ISM_NewstoreMembers_Model_Resource_Numbers extends Mage_Core_Model_Resource_Db_Abstract
 {
 
     public function _construct()
--- public/app/code/community/ISM/NewstoreMembers/Model/Resource/Numbers/Collection.php
+++ public/app/code/community/ISM/NewstoreMembers/Model/Resource/Numbers/Collection.php
@@ -1,6 +1,6 @@
 <?php
 
-class ISM_NewstoreMembers_Model_Resource_Numbers_Collection extends Mage_Core_Model_Mysql4_Collection_Abstract
+class ISM_NewstoreMembers_Model_Resource_Numbers_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
 {
 
     public function _construct()
