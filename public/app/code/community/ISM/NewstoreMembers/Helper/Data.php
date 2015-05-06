bca03a2b6a23866b30465b3fd2f2b8a02c3d3ccf
Tania Blindaruk <t.blindaruk@gmail.com>
2015-05-07 00:51:26 +0300

    NewstoreMembers:  Видаленно toOptionArray()

--- public/app/code/community/ISM/NewstoreMembers/Helper/Data.php
+++ public/app/code/community/ISM/NewstoreMembers/Helper/Data.php
@@ -5,9 +5,7 @@ class ISM_NewstoreMembers_Helper_Data extends Mage_Core_Helper_Abstract
     public function getUsers(){
         /** @var Mage_Customer_Model_Resource_Customer_Collection $customers */
         $customers = Mage::getModel('customer/customer')->getCollection()->addNameToSelect();
-
-        $customers->toOptionArray();
-
+//        $customers->toOptionArray();
         $result[]=array(
             'value'=>null,
             'label'=>'-------------------'
