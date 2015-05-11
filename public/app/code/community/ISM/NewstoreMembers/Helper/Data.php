15593c03708296a075c4daf06930bf0d5267bf86
Tania Blindaruk <t.blindaruk@gmail.com>
2015-05-12 00:48:38 +0300

    NewstoreMembers: Трішки зміненно

--- public/app/code/community/ISM/NewstoreMembers/Helper/Data.php
+++ public/app/code/community/ISM/NewstoreMembers/Helper/Data.php
@@ -2,39 +2,54 @@
 
 class ISM_NewstoreMembers_Helper_Data extends Mage_Core_Helper_Abstract
 {
-    public function getUsers(){
+    public function getUsers()
+    {
         /** @var Mage_Customer_Model_Resource_Customer_Collection $customers */
         $customers = Mage::getModel('customer/customer')->getCollection()->addNameToSelect();
-        $customers->toOptionArray();
-        $result[]=array(
-            'value'=>null,
-            'label'=>'-------------------'
+//        $customers->toOptionArray();
+        $result[] = array(
+            'value' => null,
+            'label' => '-------------------'
         );
-        foreach($customers as $customer){
+        foreach ($customers as $customer) {
             $push = array(
-                'value'=>$customer->getId(),
-                'label'=>$customer->getName()
+                'value' => $customer->getId(),
+                'label' => $customer->getName()
             );
-            $result[]=$push;
+            $result[] = $push;
         }
         return $result;
     }
 
-    public function setUserGroup($idCustomer){
+    public function setUserGroup($idCustomer)
+    {
         Mage::getModel('customer/customer')
-            ->load($idCustomer,'entity_id')
+            ->load($idCustomer, 'entity_id')
             ->setGroupId(Mage::getStoreConfig('newstoremembers/newstoremembers_group/newstoremembers_field_group'))
             ->save();
     }
 
-    public function setMemberAndGroup($number){
-        /**@var $collection ISM_NewstoreMembers_Model_Resource_Numbers_Collection*/
-        $collection =Mage::getModel('newstoremembers/numbers')->getCollection();
-        $var =$collection->getItemsByColumnValue('unique_key',$number);
-        if(isset($var[0])&&!$var[0]['customer_id']){
-            $customerId = Mage::getModel('customer/session')->getCustomer()->getEntityId();
+    private function checkNewstoreMembersGroupUser($idCustomer)
+    {
+        if (Mage::getModel('customer/customer')
+                ->load($idCustomer, 'entity_id')
+                ->getGroupId() === Mage::getStoreConfig('newstoremembers/newstoremembers_group/newstoremembers_field_group')
+        ) {
+            return false;
+        }
+        return true;
+    }
+
+    public function setMemberAndGroup($number)
+    {
+        /**@var $collection ISM_NewstoreMembers_Model_Resource_Numbers_Collection */
+        $collection = Mage::getModel('newstoremembers/numbers')->getCollection();
+        $var = $collection->getItemsByColumnValue('unique_key', $number);
+
+        $customerId = Mage::getModel('customer/session')->getCustomer()->getEntityId();
+        if (isset($var[0]) && $this->checkNewstoreMembersGroupUser($customerId) && !$var[0]['customer_id']) {
             $this->setUserGroup($customerId);
-            $var[0]['customer_id']=$customerId;
+            $var[0]['customer_id'] = $customerId;
             Mage::getModel('newstoremembers/numbers')->setData($var[0]->toArray())->save();
         }
     }
