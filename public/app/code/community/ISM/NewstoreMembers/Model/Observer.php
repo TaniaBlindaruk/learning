976c02c2812c3515ab7e497073527cdb19d4942b
t.blindaruk <t.blindaruk@gmail.com>
2015-05-12 18:38:16 +0300

    Newstore members: save order newstore members number

--- public/app/code/community/ISM/NewstoreMembers/Model/Observer.php
+++ public/app/code/community/ISM/NewstoreMembers/Model/Observer.php
@@ -3,7 +3,7 @@
 class ISM_NewstoreMembers_Model_Observer {
 
     public function checkoutTypeOnepageSaveOrderAfter(Varien_Event_Observer $observer) {
-        $observer->getQuote()->setNewstoremembersNumber(123);
-        $observer->getOrder()->setNewstoremembersNumber(123);
+        $observer->getOrder()->setNewstoremembersNumber(123)->save();
+        $observer->getQuote()->setNewstoremembersNumber(123)->save();
     }
 }
