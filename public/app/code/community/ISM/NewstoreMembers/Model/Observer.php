5377a4a10e042efcdef759fb5f568e47f4a0dca3
t.blindaruk <t.blindaruk@gmail.com>
2015-05-12 19:21:08 +0300

    Newstore members: save order newstore members number

--- public/app/code/community/ISM/NewstoreMembers/Model/Observer.php
+++ public/app/code/community/ISM/NewstoreMembers/Model/Observer.php
@@ -3,7 +3,14 @@
 class ISM_NewstoreMembers_Model_Observer {
 
     public function checkoutTypeOnepageSaveOrderAfter(Varien_Event_Observer $observer) {
-        $observer->getOrder()->setNewstoremembersNumber(123)->save();
-        $observer->getQuote()->setNewstoremembersNumber(123)->save();
+        $order = $observer->getOrder();
+        $quote=$observer->getQuote();
+        $collection=Mage::getModel('newstoremembers/numbers')->getCollection()
+            ->getItemByColumnValue('customer_id',$order->getCustomer()->getEntityId());
+        if(isset($collection[0])){
+            $order->setNewstoremembersNumber($collection[0]['unique_key'])->save();
+            $quote->setNewstoremembersNumber($collection[0]['unique_key'])->save();
+        }
+
     }
 }
