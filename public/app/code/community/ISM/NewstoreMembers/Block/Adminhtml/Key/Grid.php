800fa03a715e829726f18415885e2b45dfc03123
Tania Blindaruk <t.blindaruk@gmail.com>
2015-05-14 04:20:44 +0300

    NewstoreMembers: As a Manager I want to see customer email and name in the member numbers grid.

--- public/app/code/community/ISM/NewstoreMembers/Block/Adminhtml/Key/Grid.php
+++ public/app/code/community/ISM/NewstoreMembers/Block/Adminhtml/Key/Grid.php
@@ -3,9 +3,35 @@
 class ISM_NewstoreMembers_Block_Adminhtml_Key_Grid extends Mage_Adminhtml_Block_Widget_Grid
 {
 
-    protected function _prepareCollection()
-    {
-        $collection = Mage::getModel('newstoremembers/numbers')->getCollection();
+    protected function _prepareCollection() {
+//
+//        $collection = Mage::getModel('newstoremembers/numbers')->getCollection();
+//        $this->setCollection($collection);
+//        return parent::_prepareCollection();
+
+
+        /**@var $collection ISM_NewstoreMembers_Model_Resource_Numbers_Collection*/
+        $collection = Mage::getModel('newstoremembers/numbers')
+            ->getCollection();
+        $firstName = Mage::getModel('eav/entity_attribute')
+            ->loadByCode('1', 'firstname');
+        $lastName = Mage::getModel('eav/entity_attribute')
+            ->loadByCode('1', 'lastname');
+        $collection->getSelect()
+            ->columns(new Zend_Db_Expr("CONCAT(`cev1`.`value`, ' ',"
+                . "`cev2`.`value`) AS fullname"))
+            ->join(array('ce' => 'customer_entity'),
+                'ce.entity_id=main_table.customer_id',
+                array('email' => 'email'))
+            ->join(array('cev1' => 'customer_entity_varchar'),
+                'cev1.entity_id=main_table.customer_id',
+                array('firstname' => 'value'))
+            ->join(array('cev2' => 'customer_entity_varchar'),
+                'cev2.entity_id=main_table.customer_id',
+                array('lastname' => 'value'))
+
+            ->where('cev1.attribute_id=' . $firstName->getAttributeId())
+            ->where('cev2.attribute_id=' . $lastName->getAttributeId());
         $this->setCollection($collection);
         return parent::_prepareCollection();
     }
@@ -30,11 +56,24 @@ class ISM_NewstoreMembers_Block_Adminhtml_Key_Grid extends Mage_Adminhtml_Block_
             'type' => 'text',
         ));
 
+        $this->addColumn('fullname', array(
+            'header' => $helper->__('Customer Fullname'),
+            'index' => 'fullname',
+            'type' => 'text',
+        ));
+
+        $this->addColumn('email', array(
+            'header' => $helper->__('email'),
+            'align' => 'left',
+            'index' => 'email',
+        ));
+
         $this->addColumn('expire_date', array(
             'header' => $helper->__('Expire date'),
             'index' => 'expire_date',
             'type' => 'date',
         ));
+        return parent::_prepareColumns();
     }
     public function getRowUrl($model)
     {
