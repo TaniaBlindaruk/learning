9e89dff9f4ed02d8e1dbbefdaf083fab7d4aa2eb
t.blindaruk <t.blindaruk@gmail.com>
2015-05-20 15:59:10 +0300

    newstoremember: deleted old newstoremember

--- NewstoreMember/Model/Resource/Customer/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ISM_NewstoreMember_Model_Resource_Customer_Collection extends Mage_Customer_Model_Resource_Customer_Collection
-{
-    public function toOptionArray()
-    {
-        return parent::_toOptionArray('entity_id', 'name');
-    }
-}
